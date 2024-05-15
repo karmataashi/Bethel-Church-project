6474189ada92f2302705567529ea51879c68aa70
karmataashi <karmatashigyatsho048@gmail.com>
2024-05-15 19:33:08 +0530

    updated about us page, and updated screen size

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function reveal() {
     for(let i=0; i<reveals.length; i++) {
         let windowheight = window.innerHeight;
         let revealtop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 150;
+        let revealpoint = 100;
 
         if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
