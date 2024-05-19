d4b56d82a1de64eff3d9556f17a05a42ac9093b8
karmataashi <karmatashigyatsho048@gmail.com>
2024-05-20 00:20:01 +0530

    halka change

--- app.js
+++ app.js
@@ -36,10 +36,6 @@ function reveal() {
     }
 }
 
-
-
-
-
 const verses = [
     '"Go therefore and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit." - Matthew 28-19',
     '"I can do all things through Christ who strengthens me." - Philippians 4:13',
